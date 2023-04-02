21763d75e342f0cb9f9567560d1bcffd7162912e
Ozgundogus <ozgundogus@outlook.com>
2023-04-02 21:05:31 +0300

    create log in , sign up and splash screen

---
+++ Friendship/CreateAccountViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateAccount ViewController.swift
+//  Friendship
+//
+//  Created by Ozgun Dogus on 2.04.2023.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Friendship/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Friendship
+//
+//  Created by Ozgun Dogus on 1.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+}
+
