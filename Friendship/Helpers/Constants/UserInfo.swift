a248972ab9c277239648298ae17a8abc29ff0352
Ozgundogus <ozgundogus@outlook.com>
2023-04-04 01:08:58 +0300

    Login screen,username and password checked , alert messages

---
+++ Friendship/Helpers/Constants/UserInfo.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfo.swift
+//  Friendship
+//
+//  Created by Ozgun Dogus on 4.04.2023.
+//
+
+import Foundation
+
+enum UserInfo {
+    static let information = [
+            ("user1", "password1"),
+            ("user2", "password2"),
+            ("user3", "password3")
+        ]
+}
--- Friendship/LoginViewController.swift
+++ Friendship/LoginViewController.swift
@@ -11,23 +11,29 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
     @IBAction func loginButton(_ sender: Any) {
+        guard let username = usernameTextField.text,let password = passwordTextField.text
+        else{
+            return
+        }
+        if let _ = UserInfo.information.first(where: { $0.0 == username && $0.1 == password }){
+//            performSegue(withIdentifier: "homePage", sender: nil)
+        } else{
+            let alert = UIAlertController(title: "Hata", message: "Kullanıcı adı veya şifre hatalı", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+            alert.addAction(okAction)
+                   present(alert, animated: true, completion: nil)
+        }
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
